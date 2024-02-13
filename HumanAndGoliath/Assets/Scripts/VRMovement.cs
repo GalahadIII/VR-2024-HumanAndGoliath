090f0682ffd0def93d9e3067781656370913fa7c
razor7877 <razor7877@gmail.com>
2024-02-13 15:47:15 -0500

    Test trigger manettes VR

--- HumanAndGoliath/Assets/Scripts/VRMovement.cs
+++ HumanAndGoliath/Assets/Scripts/VRMovement.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class VRMovement : MonoBehaviour
 {
+    List<InputActionAsset> actionAssets;
+
     [SerializeField] private InputActionManager vr_input;
     void Start()
     {
-        vr_input.
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputActionAsset actionAsset = vr_input.actionAssets[0];
+        InputActionMap actionMap = actionAsset.FindActionMap("XRI RightHand Interaction");
+        InputAction action = actionMap.FindAction("Activate");
+
+        bool rHandTrigger = action.ReadValue<bool>();
+
+        if (rHandTrigger)
+            Debug.Log("Right hand trigger PRESSED");
+        else
+            Debug.Log("Right hand trigger UNPRESSED");
     }
 }
