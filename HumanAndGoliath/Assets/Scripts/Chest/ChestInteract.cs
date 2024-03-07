bf992beb0e74d83bcf27bbd2e8dde28f1b8398ba
Rockou_ <82128803+Rockouunderscore@users.noreply.github.com>
2024-03-07 09:29:45 -0500

    player cap + chest spawn platform

--- HumanAndGoliath/Assets/Scripts/Chest/ChestInteract.cs
+++ HumanAndGoliath/Assets/Scripts/Chest/ChestInteract.cs
@@ -10,12 +10,63 @@ public class ChestInteract : MonoBehaviour, IInteractable
     [SerializeField]
     private GameObject closedChest;
 
+    [SerializeField]
+    private GameObject prefab;
+
+    private GameObject spawnedItem;
+
     private bool isOpen = false;
+
+    private void Start()
+    {
+        UpdateChestVisual();
+    }
 
     public void Interact()
     {
         isOpen = !isOpen;
+
+        if (isOpen)
+        {
+            OpenChest();
+        }
+        else
+        {
+            CloseChest();
+        }
+
+    }
+
+    public void UpdateChestVisual()
+    {
         openedChest.SetActive(isOpen);
         closedChest.SetActive(!isOpen);
     }
+
+    public void OpenChest()
+    {
+        UpdateChestVisual();
+
+        if (!prefab)
+        {
+            Debug.LogError("prefab not serialized");
+            return;
+        }
+
+        if (spawnedItem)
+        {
+            Debug.Log("platform already spawned");
+            return;
+        }
+
+        Vector3 newPos = transform.position + Vector3.up * 2;
+        spawnedItem = Instantiate(prefab, newPos, Quaternion.identity, transform);
+    }
+
+    public void CloseChest()
+    {
+        UpdateChestVisual();
+
+    }
+
 }
